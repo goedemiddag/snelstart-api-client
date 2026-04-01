0ed76219af096ab7649520686f76d5642b545d5e
JirzyKerklaan <jirzykerklaan@gmail.com>
2026-04-01 16:05:08 +0200

    :sparkles: Added findAll and find methods

--- src/Request/V2/OfferteRequest.php
+++ src/Request/V2/OfferteRequest.php
@@ -9,12 +9,23 @@ namespace SnelstartPHP\Request\V2;
 
 use GuzzleHttp\Psr7\Request;
 use Psr\Http\Message\RequestInterface;
+use Ramsey\Uuid\UuidInterface;
 use SnelstartPHP\Model\V2\Offerte;
 use SnelstartPHP\Request\BaseRequest;
+use SnelstartPHP\Request\ODataRequestDataInterface;
 use SnelstartPHP\Utils;
 
 final class OfferteRequest extends BaseRequest
 {
+    public function findAll(ODataRequestDataInterface $ODataRequestData): RequestInterface
+    {
+        return new Request("GET", "offertes?" . $ODataRequestData->getHttpCompatibleQueryString());
+    }
+
+    public function findOfferte(UuidInterface $uuid): RequestInterface
+    {
+        return new Request("GET", "offertes/" . $uuid->toString());
+    }
     public function add(Offerte $offerte): RequestInterface
     {
         return new Request("POST", "offertes", [
